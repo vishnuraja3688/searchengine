c1fdcebf4f4e2269b8d7e16cf609b53cb1a880f3
vishnu raja <vishnuraja022@gmail.com>
2021-12-30 14:39:47 +0530

    three

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,7 @@ const contact =()=>{
     return(
         <div className="ui raised very padded text container segment" style={{marginTop:'80px'}}><h1>Contact us</h1>
         <p>Over 2 million free high-resolution images brought to you by the world’s
-             most generous community of photographers. You can contact us by Email!</p>
+             most generous community of photographers. You can contact us by Email</p>
         </div>
     )
 }
