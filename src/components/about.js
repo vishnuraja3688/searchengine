82b583cb5151b5d5f3fc4df766462d93db571996
vishnu raja <vishnuraja022@gmail.com>
2021-12-30 14:53:14 +0530

    five

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const about =()=>{
     return(
-        <div className="ui raised very padded text container segment" style={{marginTop:'80px'}}><h1>About Uss</h1>
+        <div className="ui raised very padded text container segment" style={{marginTop:'80px'}}><h1>About Us</h1>
         <p>Looking for some beautiful images for your website? It has you covered.
              Find the perfect website background from our massive collection of professional-quality images.
              Each is free to use!</p>
