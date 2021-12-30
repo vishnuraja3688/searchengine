d8bef6cbafa5095a63ca3c76507b6e6d6e563b4c
vishnu raja <vishnuraja022@gmail.com>
2021-12-30 14:50:56 +0530

    four

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const contact =()=>{
     return(
-        <div className="ui raised very padded text container segment" style={{marginTop:'80px'}}><h1>Contact us</h1>
+        <div className="ui raised very padded text container segment" style={{marginTop:'80px'}}><h1>Contact Us</h1>
         <p>Over 2 million free high-resolution images brought to you by the world’s
-             most generous community of photographers. You can contact us by Email</p>
+             most generous community of photographers. You can contact us by Email!</p>
         </div>
     )
 }
