186784c235d95d9b04c06fee19d9514398e74911
Vishnu Raja <87860921+vishnuraja3688@users.noreply.github.com>
2022-01-02 02:46:13 +0530

    Update Searchinput.js

--- src/components/Searchinput.js
+++ src/components/Searchinput.js
@@ -16,12 +16,18 @@ class Searchinput extends React.Component{
             <div className="ui red segment,ui raised segment,ui stacked segment,ui mini segment">
                 <form action="" onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
-                        <div className="ui massive icon input">
-                            <input type="text" placeholder="Search here..."
+                        <div className="ui huge icon input">
+                            <input type="text" placeholder="Search..."
                             onChange={(event)=>this.setState({entry: event.target.value})}
                             value={this.state.entry} />
-                        
-                            <i className="heart icon"></i>
+
+
+                       <div class="ui left corner label">
+                            <i class="asterisk icon"></i>
+                       </div>
+
+                    <i class="inverted circular search link icon"></i>
+
                         </div>
                     </div>
                 </form>
